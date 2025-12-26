722705cc4e972dcdac1a4c0901d0a2cecd5bb0c1
Sergio Perez <sergio93_bdp@hotmail.com>
2025-12-26 16:58:59 +0100

    Implemented source scrapers

--- src/Casfid/Scraper/Infrastructure/Source/Services/SourceScraper/ElMundoSourceScraper.php
+++ src/Casfid/Scraper/Infrastructure/Source/Services/SourceScraper/ElMundoSourceScraper.php
@@ -2,7 +2,9 @@
 
 namespace App\Casfid\Scraper\Infrastructure\Source\Services\SourceScraper;
 
+use App\Casfid\Scraper\Domain\Source\Model\Source;
 use App\Casfid\Scraper\Domain\Source\Model\ValueObject\SourceOrigin;
+use App\Casfid\Scraper\Domain\Source\Model\ValueObject\SourceUrl;
 use App\Casfid\Scraper\Infrastructure\Source\Services\BaseSourceScraper;
 
 class ElMundoSourceScraper extends BaseSourceScraper
@@ -19,6 +21,23 @@ class ElMundoSourceScraper extends BaseSourceScraper
 
     public function scrap(int $limit): array
     {
-        return [];
+        $crawler = $this->getCrawler($this->newsIndex());
+        $sources = [];
+
+        $crawler->filter('article')->each(function ($node) use (&$sources, $limit){
+            if(count($sources) >= $limit)
+            {
+                return;
+            }
+
+            $link = $node->filter('header a')->link();
+
+            $sources[] = Source::create(
+                new SourceUrl($link->getUri()),
+                $this->origin()
+            );
+        });
+
+        return $sources;
     }
 }
