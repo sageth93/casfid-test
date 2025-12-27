307258475c128858123bb7d92ad149a79208e3b7
Sergio Perez <sergio93_bdp@hotmail.com>
2025-12-27 18:01:32 +0100

    Swagger configured

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
